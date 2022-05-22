4b6bb85026631c33cb9898408cd0058e90dd0f14
Vladas Bukinas <vladasbukinas@gmail.com>
2022-05-22 15:14:12 +0300

    CORS revert

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*.'.config('app.domain')],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
